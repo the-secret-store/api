016a14247097349782509ec2e5c9108700adc4ed
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-07-07 08:37:15 +0530

    fixed model schema type errors

--- src/models/project.model.js
+++ src/models/project.model.js
@@ -3,8 +3,8 @@ import { model, Schema, Types } from 'mongoose';
 
 const ProjectSchema = new Schema(
 	{
-		projectName: { type: Types.String, required: true },
-		secrets: { type: Types.String }, // todo: change to map or something [OR] add a preprocess (obj.stringify)
+		projectName: { type: String, required: true },
+		secrets: { type: String }, // todo: change to map or something [OR] add a preprocess (obj.stringify)
 		owner: { type: Types.ObjectId, required: true } // think a bit..? how to co-work in projects?
 	},
 	{ timestamps: true }
--- src/models/team.model.js
+++ src/models/team.model.js
@@ -3,7 +3,7 @@ import { model, Schema, Types } from 'mongoose';
 
 const TeamSchema = new Schema(
 	{
-		teamName: { type: Types.String, required: true, trim: true },
+		teamName: { type: String, required: true, trim: true },
 		members: { type: [{ type: Types.ObjectId, ref: 'user' }] },
 		projects: { type: [{ type: Types.ObjectId, ref: 'project' }] }
 	},
