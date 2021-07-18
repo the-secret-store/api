90cf78559f7bae8718b600ce66e7b1d2645f4206
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-07-18 12:26:14 +0530

    added error stack in response on dev & test envs

--- src/tools/router.js
+++ src/tools/router.js
@@ -1,6 +1,7 @@
 import 'express-async-errors';
 import { StatusCodes } from 'http-status-codes';
 import { AuthRouter, PingRouter, UserRouter } from '@routers';
+import { config } from 'dotenv';
 
 /**
  * Registers all routes and handles server errors.
@@ -14,7 +15,10 @@ export default function registerRouters(app) {
 	app.use((err, _req, res, next) => {
 		res
 			.status(StatusCodes.INTERNAL_SERVER_ERROR)
-			.send('Something went wrong from our side. Please try again after some time.');
+			.json({
+				message: 'Something went wrong from our side. Please try again after some time',
+				error: config.util.getEnv('NODE_ENV') === 'production' ? undefined : err
+			});
 
 		next(err);
 	});
