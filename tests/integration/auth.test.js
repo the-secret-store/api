f59f0c927e20e53311415a1520fbe8f7d6581cf0
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-07-21 08:40:18 +0530

    added tests for auth route

---
+++ tests/integration/auth.test.js
@@ -0,0 +1,53 @@
+import request from 'supertest';
+import { StatusCodes } from 'http-status-codes';
+import User from '@models/user.model';
+import { validUserObject1 } from '../constants/user.constant';
+
+describe('Auth routes', () => {
+	describe('Login with email and password', () => {
+		let server;
+		beforeEach(async () => {
+			server = require('../../src/server');
+			await User.deleteMany({});
+		});
+		afterEach(async () => {
+			await server.close();
+		});
+
+		const registerUser = async payload => {
+			return await request(server).post('/user/register').send(payload);
+		};
+
+		const loginUser = async credentials => {
+			return await request(server).post('/auth/login').send(credentials);
+		};
+
+		it('should respond with 200 for correct credentials', async () => {
+			await registerUser(validUserObject1);
+			const { email, password } = validUserObject1;
+			const loginResponse = await loginUser({ email, password });
+			expect(loginResponse.statusCode).toBe(StatusCodes.OK);
+		});
+
+		it('should respond with 400 for incomplete credentials', async () => {
+			await registerUser(validUserObject1);
+			const { email } = validUserObject1;
+			const loginResponse = await loginUser({ email });
+			expect(loginResponse.statusCode).toBe(StatusCodes.BAD_REQUEST);
+		});
+
+		it('should respond with 400 for invalid credentials (validation)', async () => {
+			await registerUser(validUserObject1);
+			const { email } = validUserObject1;
+			const loginResponse = await loginUser({ email, password: 'abcdefg' });
+			expect(loginResponse.statusCode).toBe(StatusCodes.BAD_REQUEST);
+		});
+
+		it('should respond with 401 for wrong credentials', async () => {
+			await registerUser(validUserObject1);
+			const { email } = validUserObject1;
+			const loginResponse = await loginUser({ email, password: 'AbcDef12!@' });
+			expect(loginResponse.statusCode).toBe(StatusCodes.UNAUTHORIZED);
+		});
+	});
+});
