ce2ce65702ec6c49a01af7b080982cb66f410f82
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-07-23 21:34:54 +0530

    missed comments in prev. commit 🙂

--- src/middlewares/verifiedUsersOnly.js
+++ src/middlewares/verifiedUsersOnly.js
@@ -1,5 +1,9 @@
 import { StatusCodes } from 'http-status-codes';
 
+/**
+ * A middleware that forbids unverified accounts from accessing secure routes
+ * @pre-requisite: authorize middleware
+ */
 export default (req, res, next) => {
 	//* use authorization middleware
 	const { unverified } = req.user;
