3706973cc62e1707d8002f335db116dbbe9c63fe
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-07-26 08:25:55 +0530

    added index export for prettyJson

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,4 +1,5 @@
 export { default as generateOTP } from './generateOTP';
 export { default as generateRandomHex } from './generateRandomHex';
 export { default as generateRandomName } from './generateRandomName';
+export { default as prettyJson } from './prettyJson';
 export { default as sendMail } from './sendMail';
