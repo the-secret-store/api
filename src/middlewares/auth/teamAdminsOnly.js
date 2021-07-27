7002670ddea33810d7f2c4066f914fab9df94b58
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-07-27 20:23:53 +0530

    added team admins only mdlwre

---
+++ src/middlewares/auth/teamAdminsOnly.js
@@ -0,0 +1,29 @@
+import { StatusCodes } from 'http-status-codes';
+import { Team } from '@models';
+
+/**
+ * A middleware that forbids non-admin users of a team
+ * from accessing secure routes (e.g. /team/:teamId/invite, /team/:teamId/remove)
+ * @pre-requisite: authorize and verifiedAccountsOnly middlewares
+ */
+export default async (req, res, next) => {
+	//* use authorization and verifiedAccountsOnly middlewares
+	const { teamId } = req.params;
+	const { user } = req;
+
+	const team = await Team.findById(teamId);
+	if (!team) {
+		return res.status(StatusCodes.NOT_FOUND).json({ message: 'Team not found' });
+	}
+
+	if (!team.admins.includes(user.id)) {
+		return res.status(StatusCodes.FORBIDDEN).json({
+			message: 'Insufficient privileges',
+			details:
+				'You need to be an administrator to perform this operation. You are not an administrator'
+		});
+	}
+
+	req.team = team;
+	next();
+};
