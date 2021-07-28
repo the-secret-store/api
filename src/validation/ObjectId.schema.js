b8dea56842958723a007d4599232272c7a98cd78
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-07-28 14:10:42 +0530

    (feat): added JoiObjectId validation schema

---
+++ src/validation/ObjectId.schema.js
@@ -0,0 +1,11 @@
+import Joi from 'joi';
+
+/**
+ * Joi validation schema for an ObjectId
+ */
+export default Joi.string()
+	.required()
+	.pattern(new RegExp(/^[0-9a-fA-F]{24}$/), { name: 'ObjectId' })
+	.messages({
+		'string.pattern.base': 'Invalid ObjectId'
+	});
--- src/validation/index.js
+++ src/validation/index.js
@@ -4,3 +4,4 @@ export { validateProject } from '@models/project.model';
 export { default as validateAuthRequest } from './auth.validation';
 export { default as validateProjectPostRequest } from './project.validation';
 export { default as validateTeamInvite } from './teamInvite.validation';
+export { default as JoiObjectId } from './ObjectId.schema';
