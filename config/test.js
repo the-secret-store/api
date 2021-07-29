a264c243c917aa5357e2bd2c9316c5335ba67168
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-07-29 17:13:12 +0530

    setup configs for test environment

--- config/test.js
+++ config/test.js
@@ -1,5 +1,7 @@
 module.exports = {
-	loggingLevel: 'error',
+	host: '127.0.0.1',
+	port: '9090',
+	loggingLevel: 'warn',
 	logRequests: false,
 	db_uri: 'mongodb://localhost:27017/tss-test',
 	secretKey: 'test-secret',
