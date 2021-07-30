6488a32f0116477be0bc49325d25fbda46ddd429
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-07-30 21:06:30 +0530

    (test): added unit test for getRandomHex

--- tests/unit/utilities.test.js
+++ tests/unit/utilities.test.js
@@ -1,9 +1,17 @@
-import { generateOTP } from '@utilities';
+import { generateOTP, generateRandomHex } from '@utilities';
 
 describe('Utilities', () => {
 	it('OTP must be 6 digit no.', () => {
 		const otp = generateOTP();
 		expect(otp).toBeGreaterThanOrEqual(100000);
 		expect(otp).toBeLessThanOrEqual(999999);
+	});
+
+	it('Random hex', () => {
+		const SIZE = 6;
+		const randHex = generateRandomHex(SIZE);
+		const hexRegex = new RegExp(/[0-9A-Fa-f]{6}/g);
+		expect(randHex.length).toBe(SIZE);
+		expect(hexRegex.test(randHex)).toBeTruthy();
 	});
 });
