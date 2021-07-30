389b223007a681f942f9df01580a69419809fbba
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-07-30 14:05:33 +0530

    (docs/fix): added missing tags files

---
+++ src/docs/tags.docs.js
@@ -0,0 +1,16 @@
+export default {
+	tags: [
+		{
+			name: 'user',
+			description: 'User routes'
+		},
+		{
+			name: 'auth',
+			description: 'Authentication routes'
+		},
+		{
+			name: 'verify',
+			description: 'Verification routes'
+		}
+	]
+};
