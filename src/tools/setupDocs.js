84e0dc51d04b51a9e86ab8cb6ae5c1b3695cd932
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-08-11 12:30:50 +0530

    feat: added docs url info and made slug configurable

--- src/tools/setupDocs.js
+++ src/tools/setupDocs.js
@@ -1,8 +1,10 @@
 import swaggerUI from 'swagger-ui-express';
 import { name as appName, description, license, version } from 'package.json';
 import config from 'config';
+import logger from './logging';
 import docs from '@docs';
 import tags from '@docs/tags';
+import chalk from 'chalk';
 
 const documentObject = {
 	swagger: '2.0',
@@ -19,6 +21,11 @@ const documentObject = {
 };
 
 export default function setupDocs(app) {
-	if (config.get('serveDocument'))
-		app.use('/docs', swaggerUI.serve, swaggerUI.setup(documentObject));
+	if (!config.get('docs.serve')) return;
+
+	const address = config.get('host');
+	const port = config.get('port');
+	const docsSlug = config.get('docs.slug');
+	logger.info(`Serving docs at ${chalk.cyan(`http://${address}:${port}${docsSlug}`)}`);
+	app.use(docsSlug, swaggerUI.serve, swaggerUI.setup(documentObject));
 }
